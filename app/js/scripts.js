9217990a8971a294e29ffe7e7cac104902757d52
justdealman <justdealman@gmail.com>
2021-06-28 09:24:38 +0700

    main/afisha/repertoire

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -23,20 +23,23 @@
 		}
 	});
 
+    var arrowBgPrev = '<button class="slick-arrow slick-prev">' +
+            '<svg width="70" height="16" viewBox="0 0 70 16" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+              '<path d="M0.292892 8.70711C-0.0976334 8.31658 -0.0976334 7.68342 0.292892 7.29289L6.65685 0.928932C7.04738 0.538408 7.68054 0.538408 8.07107 0.928932C8.46159 1.31946 8.46159 1.95262 8.07107 2.34315L2.41422 8L8.07107 13.6569C8.46159 14.0474 8.46159 14.6805 8.07107 15.0711C7.68054 15.4616 7.04738 15.4616 6.65685 15.0711L0.292892 8.70711ZM70 9H1V7H70V9Z"/>' +
+            '</svg>' +
+        '</button>';
+    var arrowBgNext = '<button class="slick-arrow slick-next">' +
+            '<svg width="70" height="16" viewBox="0 0 70 16" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+                '<path d="M69.7071 8.70711C70.0976 8.31658 70.0976 7.68342 69.7071 7.29289L63.3431 0.928932C62.9526 0.538408 62.3195 0.538408 61.9289 0.928932C61.5384 1.31946 61.5384 1.95262 61.9289 2.34315L67.5858 8L61.9289 13.6569C61.5384 14.0474 61.5384 14.6805 61.9289 15.0711C62.3195 15.4616 62.9526 15.4616 63.3431 15.0711L69.7071 8.70711ZM0 9H69V7H0V9Z"/>' +
+            '</svg>' +
+        '</button>';
+
     $('.js-slider-juri').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: true,
-        prevArrow: '<button class="slick-arrow slick-prev">' +
-                '<svg width="70" height="16" viewBox="0 0 70 16" fill="none" xmlns="http://www.w3.org/2000/svg">' +
-					'<path d="M0.292892 8.70711C-0.0976334 8.31658 -0.0976334 7.68342 0.292892 7.29289L6.65685 0.928932C7.04738 0.538408 7.68054 0.538408 8.07107 0.928932C8.46159 1.31946 8.46159 1.95262 8.07107 2.34315L2.41422 8L8.07107 13.6569C8.46159 14.0474 8.46159 14.6805 8.07107 15.0711C7.68054 15.4616 7.04738 15.4616 6.65685 15.0711L0.292892 8.70711ZM70 9H1V7H70V9Z"/>' +
-				'</svg>' +
-            '</button>',
-        nextArrow: '<button class="slick-arrow slick-next">' +
-                '<svg width="70" height="16" viewBox="0 0 70 16" fill="none" xmlns="http://www.w3.org/2000/svg">' +
-					'<path d="M69.7071 8.70711C70.0976 8.31658 70.0976 7.68342 69.7071 7.29289L63.3431 0.928932C62.9526 0.538408 62.3195 0.538408 61.9289 0.928932C61.5384 1.31946 61.5384 1.95262 61.9289 2.34315L67.5858 8L61.9289 13.6569C61.5384 14.0474 61.5384 14.6805 61.9289 15.0711C62.3195 15.4616 62.9526 15.4616 63.3431 15.0711L69.7071 8.70711ZM0 9H69V7H0V9Z"/>' +
-				'</svg>' +
-            '</button>',
+        prevArrow: arrowBgPrev,
+        nextArrow: arrowBgNext,
         dots: false,
         cssEase: 'ease',
         speed: 1000,
@@ -96,6 +99,55 @@
     	var id = parseInt($(this).attr('data-id'))-1;
 		$('.js-gallery-main').slick('slickGoTo', id);
 	});
+
+    var $introSlider = $('.js-intro');
+    $introSlider.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: arrowBgPrev,
+        nextArrow: arrowBgNext,
+        dots: true,
+        cssEase: 'ease',
+        speed: 1000,
+        infinite: true,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 9000,
+        pauseOnHover: false,
+        pauseOnDotsHover: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: false
+                }
+            }
+        ]
+    });
+
+    var $repertoireSlider = $('.js-repertoire');
+    $repertoireSlider.slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: arrowBgPrev,
+        nextArrow: arrowBgNext,
+        dots: false,
+        cssEase: 'ease',
+        speed: 1000,
+        infinite: true,
+        adaptiveHeight: true,
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                variableWidth: true
+            }
+        }
+    ]
+    });
 
     function startApp() {
     }
